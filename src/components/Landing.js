8798168a784a3236fef40d9417055ef4a01ba752
Akino Watanabe <akino.wata@gmail.com>
2019-09-15 04:24:36 -0400

    UI improvements

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -14,10 +14,11 @@ class Landing extends React.Component {
     render() {
         return (
             <div className='landing'>
-                <h4>Meetings demand your undivided attention.</h4>
-                <h3>SlackR gives you your life back.</h3>
-
-                <p>Summarize meeting transcripts and receive notifications when you people call your name.</p>
+                <h4>Meetings demand your undivided attention, but...</h4>
+                <h3>SlackR gives you your life back!</h3>
+                <p>You will not be trapped on a chair in online meetings anymore.</p>
+                <p>You can even accidentally fall asleep during online voice meetings, and that's fine!</p> 
+                <p>SlackR will summarize meeting transcripts and will notify you when people call your name.</p>
             </div>
         );
     }
